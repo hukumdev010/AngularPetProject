585d0958353ffd330af76385b1585627ccccfff0
Hukum Basnet <hukumbasnet@lftechnology.com>
2024-02-05 21:03:26 +0545

    Add files for crud

---
+++ src/app/crud/curd.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurdService {
+  private apiServer = "http://localhost:3000";
+
+  constructor(private httpClient: HttpClient) { }
+}
